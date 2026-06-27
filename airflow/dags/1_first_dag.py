f9266a7ca6a00821a4c0040d82712edfae077197
benitmatumona <polviedivine@gmail.com>
2026-06-27 19:24:42 +0200

    completed basics for first dag

--- airflow/dags/1_first_dag.py
+++ airflow/dags/1_first_dag.py
@@ -1,17 +1,24 @@
 from airflow.sdk import dag, task
 
 
-
+@dag(
+        dag_id= first_dag
+)
 def first_dag():
     
     @task.python
     def first_task():
         print("this is the first task")
     
+    @task.python
     def second_task():
         print("this is the first task")
     
+    @task.python
     def third_task():
         print("this is the first task")
     
     first = first_task()
+    second = second_task()
+    third = third_task()
+    first >> second >> third
